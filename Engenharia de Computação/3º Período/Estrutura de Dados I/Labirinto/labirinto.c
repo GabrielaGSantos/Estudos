052d029f99912ef578261424bd284d638c4c953b
Vitor Bruno de Oliveira Barth <vbob@vbob.com.br>
2016-08-30 10:52:43 -0400

    Update 30-08-2016

--- Engenharia de Computação/3º Período/Estrutura de Dados I/Labirinto/labirinto.c
+++ Engenharia de Computação/3º Período/Estrutura de Dados I/Labirinto/labirinto.c
@@ -4,7 +4,7 @@
 // Developed by Luís Alexandre Ferreira Bueno and Vitor Bruno de Oliveira Barth
 // Em 24/08/2016
 
-#include "iQueue.h"
+#include "xyQueue.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -16,19 +16,11 @@
 void writePPM(int maze[SIZEW][SIZEH], char *name);
 void solveMaze(int maze[SIZEW][SIZEH], int startPosition[2], int finalPosition[2]);
 
-void imprimirMatriz(int matriz[SIZEW][SIZEH]) {
-	for (int i = 0; i < SIZEW; i++) {
-		for (int j = 0; j< SIZEH; j++)
-			printf("%2d ", matriz[i][j]);
-		printf("\n");
-	}
-}
-
 int main() {
 	int maze[SIZEW][SIZEH] = {
-		  	{1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-		  	{1, 0, 0, 0, 0, 1, 0, 0, 1, 0},
-			{0, 1, 1, 1, 0, 1, 0, 1, 1, 0},
+		  	{1, 1, 1, 0, 1, 1, 1, 1, 1, 0},
+		  	{1, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
 			{1, 0, 1, 0, 0, 1, 1, 0, 1, 1},
 			{1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
 			{1, 1, 0, 0, 1, 1, 1, 0, 1, 1},
@@ -37,46 +29,45 @@ int main() {
 			{1, 1, 0, 1, 0, 1, 1, 1, 0, 1},
 			{1, 1, 0, 1, 0, 1, 1, 1, 0, 1}
 	}; // 1 stands for WAY and 0 stands for BLOCKAGE
-	
+
 	writePPM(maze, "NONSOLVED.ppm");
 
 	int startPosition[2] = {0,0};
-	int finalPosition[2] = {(SIZEH-1),(SIZEW-1)};
-	
-	solveMaze(maze, startPosition, finalPosition);
+	int finalPosition[2] = {(SIZEW-1),(SIZEH-1)};
 
+	solveMaze(maze, startPosition, finalPosition);
 }
 
 void writePPM(int maze[SIZEW][SIZEH], char *name) {
 	int resolutionWidth = 800; // Width of the generated image
 	int resolutionHeight = 800; // Height of the generated image
 
-	int blockSizeHeight = (int) resolutionHeight/SIZEH;
-	int blockSizeWidth = (int) resolutionWidth/SIZEW;
+	int blockSizeHeight = (int) resolutionHeight/SIZEH; // Proportional Height of each Block
+	int blockSizeWidth = (int) resolutionWidth/SIZEW; // Proportional Width of each Block
 
 	FILE *image = fopen(name, "w");
 
 	if (image == NULL)
 		printf("Error opening file!\n");
 
-	char *header[4] = {
+	// CREATES HEADER OF PPM
+	char *header[2] = {
 		"P3",
 		"# P3 PPM Image Format",
-		"# Using 1 for color in RGB format"
 	};
 
-	fprintf(image, "%s\n%s\n%s\n%d %d 1\n", header[0], header[1], header[2], resolutionWidth, resolutionHeight);
+	fprintf(image, "%s\n%s\n%d %d 1\n", header[0], header[1], resolutionWidth, resolutionHeight);
 
-	for (int i = 0; i < SIZEH; i++) {
-		for (int j = 0; j < blockSizeHeight; j++) {
-			for (int k = 0; k < SIZEW; k++) {
-				for (int l = 0; l < blockSizeWidth; l++) {
+	for (int i = 0; i < SIZEH; i++) { // ITERATES MAZE LINES
+		for (int j = 0; j < blockSizeHeight; j++) { // ITERATES EACH LINE PIXELS HEIGHT
+			for (int k = 0; k < SIZEW; k++) { // ITERATES MAZE COLUMNS
+				for (int l = 0; l < blockSizeWidth; l++) { // ITERATES EACH COLUMN PIXEL WIDTH
 					if (maze[i][k] == 0)
-						fprintf(image, "0 0 0  ");
+						fprintf(image, "0 0 0  "); // PAINT WALL
 					else if (maze[i][k] == -1 && (j > blockSizeHeight/3 && j < 2*blockSizeHeight/3 || l > blockSizeHeight/3 && l < 2*blockSizeHeight/3))
-						fprintf(image, "1 0 0  ");
+						fprintf(image, "1 0 0  "); // PAINT CROSS IN WAY OUT
 					else
-						fprintf(image, "1 1 1  ");
+						fprintf(image, "1 1 1  "); // PAINT WAY
 				}
 			}
 		fprintf(image, "\n");
@@ -87,91 +78,79 @@ void writePPM(int maze[SIZEW][SIZEH], char *name) {
 }
 
 void solveMaze(int maze[SIZEW][SIZEH], int startPosition[2], int finalPosition[2]) {
-	iQueue X, Y;
+	xyQueue queue;
+	init(&queue,1);
+	enqueue(&queue,(coordXY){startPosition[1],startPosition[0]});
 
-	init(&X, 0);
-	init(&Y, 0);
+	int wayOut=0;
 
-	enqueue(&X, startPosition[0]);
-	enqueue(&Y, startPosition[1]);
+	maze[startPosition[1]][startPosition[0]] = 2;
 
-	maze[peek(&X)][peek(&Y)] = 2;
+	// USED TO FIND WAY OUT
+	while (!isEmpty(&queue) && !wayOut) {
+		coordXY pos = dequeue(&queue);
 
-	do {
+		int way = maze[pos.x][pos.y];
 
-		int trilha = maze[peek(&X)][peek(&Y)];
+		if (pos.x==finalPosition[1] && pos.y==finalPosition[0]) {
+			wayOut = 1;
+		} 
 
-		if(!isEmpty(&X) && (maze[peek(&X)+1][peek(&Y)] == 1  && peek(&X)+1 < SIZEH)) {
-			maze[peek(&X)+1][peek(&Y)] = trilha+1;
-			enqueue(&X, peek(&X)+1);
-			enqueue(&Y, peek(&Y));	
+		else {
+			if (pos.x < SIZEW-1 && maze[pos.x+1][pos.y]==1) { 
+				enqueue(&queue,(coordXY){pos.x+1,pos.y}); 
+				maze[pos.x+1][pos.y]=way+1;
+			}
+
+			if (pos.y < SIZEH-1 && maze[pos.x][pos.y+1]==1) { 
+				enqueue(&queue,(coordXY){pos.x,pos.y+1}); 
+				maze[pos.x][pos.y+1]=way+1;
+			}
+
+			if (pos.y > 0 && maze[pos.x][pos.y-1]==1) { 
+				enqueue(&queue,(coordXY){pos.x,pos.y-1}); 		  
+				maze[pos.x][pos.y-1]=way+1;
+			}
+		
+			if (pos.x > 0 && maze[pos.x-1][pos.y]==1) { 
+				enqueue(&queue,(coordXY){pos.x-1,pos.y});
+				maze[pos.x-1][pos.y]=way+1;}
 		}
-
-		if(!isEmpty(&X) && (maze[peek(&X)-1][peek(&Y)] == 1 && peek(&X)-1 >= 0)) {
-			maze[peek(&X)-1][peek(&Y)] = trilha+1;
-			enqueue(&X, peek(&X)-1);
-			enqueue(&Y, peek(&Y));	
-		}
-
-		if(!isEmpty(&X) && (maze[peek(&X)][peek(&Y)+1] == 1 && peek(&Y)+1 < SIZEW)) {
-			maze[peek(&X)][peek(&Y)+1] = trilha+1;
-			enqueue(&X, peek(&X));
-			enqueue(&Y, peek(&Y)+1);	
-		}	
-	
-		if(!isEmpty(&X) && (maze[peek(&X)][peek(&Y)-1] == 1 && peek(&Y)-1 >= 0)) {
-			maze[peek(&X)][peek(&Y)-1] 	= trilha+1;
-			enqueue(&X, peek(&X));
-			enqueue(&Y, peek(&Y)-1);	
-		}
-
-		if (!isEmpty(&X)) 
-			maze[dequeue(&X)][dequeue(&Y)] = trilha;
-
-		if (maze[finalPosition[0]][finalPosition[1]] != 1)
-			break;
-
-	} while (!isEmpty(&X));
-
-	if (maze[finalPosition[0]][finalPosition[1]] == 1)
-		printf("IMPOSSIBLE TO FIND A WAY FROM (%d, %d) to (%d, %d)\n\n", startPosition[0], startPosition[1], finalPosition[0], finalPosition[1]);
-
-	enqueue(&X, finalPosition[0]);
-	enqueue(&Y, finalPosition[1]);
-
-	imprimirMatriz(maze);
-
-	while (maze[peek(&X)][peek(&Y)] != 2) {
-
-		int trilha = maze[peek(&X)][peek(&Y)];
-
-		if(!isEmpty(&X) && (maze[peek(&X)+1][peek(&Y)] == maze[peek(&X)][peek(&Y)]-1  && peek(&X)+1 < SIZEH)) {
-			enqueue(&X, peek(&X)+1);
-			enqueue(&Y, peek(&Y));	
-		}
-
-		if(!isEmpty(&X) && (maze[peek(&X)-1][peek(&Y)] == maze[peek(&X)][peek(&Y)]-1 && peek(&X)-1 >= 0)) {
-			enqueue(&X, peek(&X)-1);
-			enqueue(&Y, peek(&Y));	
-		}
-
-		if(!isEmpty(&X) && (maze[peek(&X)][peek(&Y)+1] == maze[peek(&X)][peek(&Y)]-1 && peek(&Y)+1 < SIZEW)) {
-			enqueue(&X, peek(&X));
-			enqueue(&Y, peek(&Y)+1);	
-		}	
-	
-		if(!isEmpty(&X) && (maze[peek(&X)][peek(&Y)-1] == maze[peek(&X)][peek(&Y)]-1 && peek(&Y)-1 >= 0)) {
-			enqueue(&X, peek(&X));
-			enqueue(&Y, peek(&Y)-1);	
-		}
-
-		if (!isEmpty(&X)) 
-			maze[dequeue(&X)][dequeue(&Y)] =  -1;
 	}
 
-	maze[startPosition[0]][startPosition[1]] = -1;
+	if (wayOut) {
+		empty(&queue); // USED TO EMPTY THE QUEUE
 
-	writePPM(maze, "SOLVED.ppm");
+		enqueue(&queue,(coordXY){finalPosition[1],finalPosition[0]});
+		wayOut = 0;
 
-	imprimirMatriz(maze);
+		// IF THERE'S A WAY OUT, GO BACK TO FIND THE SHORTEST
+		while (!wayOut) {
+			coordXY pos = dequeue(&queue);
+
+			if (pos.x < SIZEW-1 && maze[pos.x+1][pos.y]==maze[pos.x][pos.y]-1)
+				enqueue(&queue,(coordXY){pos.x+1,pos.y});
+
+			if (pos.y < SIZEH-1 && maze[pos.x][pos.y+1]==maze[pos.x][pos.y]-1) 
+				enqueue(&queue,(coordXY){pos.x,pos.y+1}); 
+
+			if (pos.y > 0 && maze[pos.x][pos.y-1]==maze[pos.x][pos.y]-1) 
+				enqueue(&queue,(coordXY){pos.x,pos.y-1}); 		  
+
+			if (pos.x > 0 && maze[pos.x-1][pos.y]==maze[pos.x][pos.y]-1)
+				enqueue(&queue,(coordXY){pos.x-1,pos.y});
+
+			if (pos.x==startPosition[1] && pos.y==startPosition[0]) {
+				wayOut = 1;
+				maze[pos.x][pos.y]=-1;
+			}
+
+			maze[pos.x][pos.y]=-1;
+		}
+
+		writePPM(maze, "SOLVED.ppm");
+	} 
+
+	else 
+		printf("\n\nNO WAY FROM (%d, %d) TO (%d, %d)\n\n", startPosition[0], startPosition[1], finalPosition[0], finalPosition[1]);
 }
